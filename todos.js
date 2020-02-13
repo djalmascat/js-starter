e346e7f4bc06c9e5fe3ea95285d8c2bf2c35e613
djalmascat <djalmascat@gmail.com>
2020-02-12 22:10:40 -0300

    .

--- todos.js
+++ todos.js
@@ -1,23 +1,52 @@
-var btnElement = document.querySelector('#btn');
-        btnElement.setAttribute('onclick', 'newBox()');
+//var btnElement = document.querySelector('#btn');
+//      btnElement.setAttribute('onclick', 'newBox()');
 
-        var newBox = function () {
-            var boxElement = document.createElement('div');
-            boxElement.style.width = '100px';
-            boxElement.style.height = '100px';
-            boxElement.style.backgroundColor = '#f00';
-            boxElement.setAttribute('onmouseover', 'style.backgroundColor=newColor()');
+//        var newBox = function () {
+//          var boxElement = document.createElement('div');
+//        boxElement.style.width = '100px';
+//      boxElement.style.height = '100px';
+//    boxElement.style.backgroundColor = '#f00';
+//  boxElement.setAttribute('onmouseover', 'style.backgroundColor=newColor()');
 
-            var containerApp = document.querySelector('#app');
-            containerApp.appendChild(boxElement);
+//var containerApp = document.querySelector('#app');
+//containerApp.appendChild(boxElement);
 
-        }
+//}
 
-        var newColor = function getRandomColor() {
-            var letters = "0123456789ABCDEF";
-            var color = "#";
-            for (var i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-            return color;
-        }
+//var newColor = function getRandomColor() {
+//   var letters = "0123456789ABCDEF";
+// var color = "#";
+//for (var i = 0; i < 6; i++) {
+//   color += letters[Math.floor(Math.random() * 16)];
+//}
+//return color;
+//}
+
+listElement = document.querySelector('#app ul');
+inputElement = document.querySelector('#app input');
+buttonElement = document.querySelector('#app button');
+
+var todos = [
+    'Fazer café',
+    'Estudar Javascript',
+    'Acessar comunidade freecodecamp'
+];
+
+function renderTodos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+
+function addTodo(){
+    var todoText = inputElement.value;
+    todos.push(todoText);
+    input
+}
